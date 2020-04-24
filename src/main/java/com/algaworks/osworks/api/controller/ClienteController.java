1303d673a1593d7ca51a92dc6b48d566043d3684
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-04-23 21:33:47 -0300

    Aula 01 - Implementando uma REST API com Spring

---
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -0,0 +1,32 @@
+package com.algaworks.osworks.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.osworks.domain.model.Cliente;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@GetMapping
+	public List<Cliente> listar() {
+		var cliente1 = new Cliente();
+		cliente1.setId(1L);
+		cliente1.setNome("João");
+		cliente1.setTelefone("34 99999-1111");
+		cliente1.setEmail("joaodascouves@algaworks.com");
+		
+		var cliente2 = new Cliente();
+		cliente2.setId(1L);
+		cliente2.setNome("Maria");
+		cliente2.setTelefone("11 77777-1111");
+		cliente2.setEmail("mariadasilva@algaworks.com");
+		return Arrays.asList(cliente1, cliente2);
+	}
+
+}
